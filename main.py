7cdc0135055870572427a20ff8d51e9f3f42edfc
Miles Contopoulos <milessayshi7@gmail.com>
2023-08-17 12:33:22 -0400

    switched to pygame for pause capability

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-import os, random
-import time
+import os, random, time, pygame
 
-from playsound import playsound
 from mutagen.mp3 import MP3
 
 musicpath = os.path.dirname(os.path.realpath(__file__)) + "\\music\\"
 music = os.listdir(musicpath)
+
+pygame.init()
+pygame.mixer.music.set_volume(1.0)
 
 previous = ""
 endTime = 0
 
 while True:
     if endTime < int(time.time()):
+        pygame.mixer.music.unload()
         choice = random.choice(music)
         while choice == previous:
             choice = random.choice(music)
         previous = choice
         audio = MP3(str(musicpath + choice))
-        print(f"{choice} ({int(audio.info.length)}s)")
-        playsound(str(musicpath + choice), False)
+        print(f"Playing: {choice[:-4]} ({int(audio.info.length / 60)}:{int(audio.info.length % 60)})")
         endTime = int(time.time()) + audio.info.length
+        pygame.mixer.music.load(musicpath + choice)
+        pygame.mixer.music.play(1)
